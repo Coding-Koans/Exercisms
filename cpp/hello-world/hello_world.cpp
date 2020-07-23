28fc80133576d0ef02756594b736e080a8fb4561
Ernesto Joaquine <joaquine89@gmail.com>
2020-07-22 18:33:02 -0700

    Make hello function say hello
      - also make a giant branch happen in backyard
    
    Signed-off-by: Ian Carroll <14797009+IanDCarroll@users.noreply.github.com>

--- cpp/hello-world/hello_world.cpp
+++ cpp/hello-world/hello_world.cpp
@@ -1,16 +1,14 @@
 #include "hello_world.h"
 
-// Use everything from the 'std' namespace.
-// This lets us write 'string' instead of 'std::string'.
 using namespace std;
 
 namespace hello_world {
 
-// Define the function itself. This could have also been written as:
-// std::string hello_world::hello()
-string hello() {
-    // Return the string we need.
-    return "Fix me!";
-}
+    string hello() {
+        return "Hello, World!";
+    }
 
-}  // namespace hello_world
+    string backyard() {
+        return "OMG! There's a giant branch!";
+    }
+}
--- cpp/hello-world/hello_world_test.cpp
+++ cpp/hello-world/hello_world_test.cpp
@@ -1,12 +1,13 @@
-// Include the header file with the definitions of the functions you create.
 #include "hello_world.h"
 
-// Include the test framework.
 #include "test/catch.hpp"
 
-// Declares a single test.
 TEST_CASE("test_hello")
 {
-    // Check if your function returns "Hello, World!".
     REQUIRE(hello_world::hello() == "Hello, World!");
+}
+
+TEST_CASE("test_backyard")
+{
+    REQUIRE(hello_world::backyard() == "OMG! There's a giant branch!");
 }
