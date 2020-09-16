f540899726f8ae1053439464fc13ad8def901632
Ian Carroll <14797009+IanDCarroll@users.noreply.github.com>
2020-09-16 15:04:25 -0700

    Refactor tests to be precicely 3 lines long (Arange, Act, Assert)

--- python/reverse-string/reverse_string_test.py
+++ python/reverse-string/reverse_string_test.py
@@ -6,38 +6,32 @@ class ReverseStringTest(unittest.TestCase):
     def test_an_empty_string(self):
         given = ""
         actual = reverse(given)
-        expected = ""
-        self.assertEqual(actual, expected)
+        self.assertEqual(actual, "")
 
     def test_a_word(self):
         given = "robot"
         actual = reverse(given)
-        expected = "tobor"
-        self.assertEqual(actual, expected)
+        self.assertEqual(actual, "tobor")
 
     def test_a_capitalized_word(self):
         given = "Ramen"
         actual = reverse(given)
-        expected = "nemaR"
-        self.assertEqual(actual, expected)
+        self.assertEqual(actual, "nemaR")
 
     def test_a_sentence_with_punctuation(self):
         given = "I'm hungry!"
         actual = reverse(given)
-        expected = "!yrgnuh m'I"
-        self.assertEqual(actual, expected)
+        self.assertEqual(actual, "!yrgnuh m'I")
 
     def test_a_palindrome(self):
         given = "racecar"
         actual = reverse(given)
-        expected = "racecar"
-        self.assertEqual(actual, expected)
+        self.assertEqual(actual, "racecar")
 
     def test_an_even_sized_word(self):
         given = "drawer"
         actual = reverse(given)
-        expected = "reward"
-        self.assertEqual(actual, expected)
+        self.assertEqual(actual, "reward")
 
 
 if __name__ == "__main__":
