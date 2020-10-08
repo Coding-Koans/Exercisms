2ee54466291f4ce73374f8c25814279bc9dc55df
Brian Sung <6445410+seoulection@users.noreply.github.com>
2020-10-07 18:23:28 -0700

    Get the Basics of Leap years implemented sanely
    Co-authored-by: Ian Carroll <14797009+IanDCarroll@users.noreply.github.com>

--- elixir/leap/test/leap_test.exs
+++ elixir/leap/test/leap_test.exs
@@ -1,14 +1,24 @@
 defmodule LeapTest do
   use ExUnit.Case
 
-  # @tag :pending
   test "vanilla leap year" do
     assert Year.leap_year?(1996)
   end
 
-  @tag :pending
   test "any old year" do
     refute Year.leap_year?(1997), "1997 is not a leap year."
+  end
+
+  test "another any old year" do
+    refute Year.leap_year?(1987), "1987 is not a leap year."
+  end
+
+  test "yet another any old year" do
+    refute Year.leap_year?(1487), "1487 is not a leap year."
+  end
+
+  test "an even year that is not a leap year" do
+    refute Year.leap_year?(1998), "1998 is not a leap year."
   end
 
   @tag :pending
